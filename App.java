eb2056f4f022069639b1d14ec40f25de85003afc
Ravi Melinkeri <ravimelinikeribs@gmail.com>
2025-12-17 13:42:43 +0530

    variables and some questions solved

--- App.java
+++ App.java
@@ -1,6 +1,83 @@
+import java.util.Scanner;
+
 public class App{
     public static void main(String[] args){
-        System.out.println("Hello World");
-        System.out.print("JAVA");
+       
+
+        
+        // 01 Declear the Int variable with name age and value 20
+        
+        //int age = 20;
+        //System.out.println(age); //20
+        
+        // Declare a double variable salary and assign 45000.75.
+        //double salary = 45000.75;
+        //System.out.println(salary); // 45000.75
+        
+        //Declare a char variable to store the first letter of your name
+        
+        //char Lat = 'R';
+        //System.out.println(Lat);
+        
+        //boolen
+        
+        //boolean isjavaeasy = true;
+        //System.out.println(isjavaeasy);
+        
+        // String fovLang = "Java";
+        // System.out.println(fovLang);
+        
+        
+        // int a = 1; int b = 2; int c = 3;
+        // System.out.println(a);
+        // System.out.println(b);
+        
+        
+        // int a = 12;
+        // double b = 2.29;
+        
+        // // int Intsum = a + b;
+        // double Dobsum = a + b;
+        
+        
+        // System.out.println(a + b); // 14.29
+        // // System.out.println(Intsum); //14
+        // System.out.println(Dobsum); //14.29
+        
+        // int a = 10;
+        // int b = 20;
+        // int c;
+        // c=a; //c=10
+        // a=b; // a=20
+        // b=c; // b=10
+        
+        
+        // System.out.println(a);
+        // System.out.println(b);
+       
+    //   String name = "Ravichandra";
+    //   int age = 23;
+    //   double height = 145.67;
+       
+    //   System.out.println(name);
+    //   System.out.println(age);
+    //   System.out.println(height);
+    
+    // Scanner sc = new Scanner(System.in);
+    // int a = sc.nextInt();
+    // int b = sc.nextInt();
+    // System.out.println(a + b);
+    
+    //12 Write a program to calculate the area of a rectangle using variables.
+    
+    // Scanner sc = new Scanner(System.in);
+    // int a = sc.nextInt();
+    // int b = sc.nextInt();
+    
+    // System.out.println(a*b);
+        
+        
+    
     }
 }
+
